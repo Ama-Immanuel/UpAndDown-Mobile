465185f6b89734609f5ab0de2478409f86e84cf7
andreaszico <zicoaritonang@gmail.com>
2023-06-12 19:38:31 +0700

    add more color basic

--- lib/cores/styles/colors.dart
+++ lib/cores/styles/colors.dart
@@ -6,4 +6,9 @@ class AppColors {
   var bluePrimary = const Color(0xff3F72AF);
   var blueSoft = const Color(0xffDBE2EF);
   var blueDark = const Color(0xff112D4E);
+
+  var yellowPrimary = const Color(0xffF2BE22);
+  var redPrimary = const Color(0xffEB1D36);
+  var greenPrimary = const Color(0xff3CCF4E);
+
 }
